51d68c781a716f7412eaa570e77dee6d1e022d35
Michael DiPasquale <michaeldipasquale313@gmail.com>
2025-02-14 16:09:57 -0500

    date bug

--- frontend/src/components/BudgetTracker.js
+++ frontend/src/components/BudgetTracker.js
@@ -63,9 +63,9 @@ function BudgetTracker({ userId }) {
         year: "numeric",
         month: "long",
         day: "numeric",
-        // hour: "numeric",
-        // minute: "numeric",
-        // second: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
       });
       const newLog = { note: `New budget set to $${budget}`, date: currentDate };
       
